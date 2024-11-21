0261b2fc13d5840ca7c1b6ee785f2db5e7a418d9
Zias-Git <j.adam@htp.net>
2024-11-21 22:51:32 +0100

    tried to implement a route for readAll cats wrapped all methods in CatService with if em=null stuff

---
+++ src/main/java/org/rosinenhasser/jakarta/CatController/CatControllerServlet.java
@@ -0,0 +1,45 @@
+package org.rosinenhasser.jakarta.CatController;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.rosinenhasser.jakarta.cat.CatEntity;
+import org.rosinenhasser.jakarta.cat.CatService;
+
+import jakarta.inject.Inject;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("/getCats")
+public class CatControllerServlet extends HttpServlet {
+
+    @Inject
+    private CatService catService;
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+        throws ServletException, IOException {
+            System.out.println("Test");
+            System.out.println(request);
+            System.out.println(response);
+            // get cats
+            CatService catService = new CatService();
+            List<CatEntity> catEntities = new ArrayList<CatEntity>();
+            catEntities = catService.readAll();
+
+            // Setzen des Inhalts-Typs der Antwort (z.B. HTML)
+            response.setContentType("text/html");
+            // Writer zum Schreiben der Antwort holen
+            PrintWriter out = response.getWriter();
+            // HTML-Ausgabe generieren
+            out.println(catEntities.toString());
+
+
+        }
+
+}
--- src/main/java/org/rosinenhasser/jakarta/cat/CatService.java
+++ src/main/java/org/rosinenhasser/jakarta/cat/CatService.java
@@ -1,5 +1,6 @@
 package org.rosinenhasser.jakarta.cat;
 
+import jakarta.annotation.Nullable;
 import jakarta.ejb.Stateless;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
@@ -7,23 +8,36 @@ import java.util.List;
 
 @Stateless
 public class CatService {
+    @Nullable
     @PersistenceContext(unitName = "cats")
     private EntityManager em;
 
     public void create(CatEntity entity) {
-        em.persist(entity);
+        if (em != null) {
+            em.persist(entity);
+        }
     }
 
     public CatEntity read(Long id) {
-        return em.find(CatEntity.class, id);
+        if (em != null) {
+            return em.find(CatEntity.class, id);
+        } else {
+            throw new IllegalArgumentException("EntityManager ist null");
+        }
     }
 
     public List<CatEntity> readAll() {
-        return em.createQuery("SELECT e FROM CatEntity e", CatEntity.class).getResultList();
+        if (em != null) {
+            return em.createQuery("SELECT e FROM CatEntity e", CatEntity.class).getResultList();
+        } else {
+            throw new IllegalArgumentException("EntityManager ist null");
+        }
     }
 
     public void update(CatEntity entity) {
-        em.merge(entity);
+        if (em != null) {
+            em.merge(entity);
+        }
     }
 
     public void delete(Long id) {
